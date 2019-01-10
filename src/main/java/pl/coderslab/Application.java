13980a21dcb75f2ee157e20b6c1c90b5c691ea59
Krzysiekkwiatkowski <krzyszt.kwiatkowski@gmail.com>
2019-01-10 17:10:22 +0100

    Changed some styles and added basic.css file

--- src/main/java/pl/coderslab/Application.java
+++ src/main/java/pl/coderslab/Application.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
